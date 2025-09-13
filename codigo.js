4fe0ea9563c8ee02a715d949f961bf1bd0e53ad9
Jesus Eduardo Ruiz Ramirez <jruiz@cajacentro.com.pe>
2025-09-13 11:39:49 -0500

    Proyecto tempracker version final

--- codigo.js
+++ codigo.js
@@ -2,6 +2,18 @@ const claveApi = '6c6f1fb43c16475c8aa154946250709';
 const idioma = 'es';
 const inpCiudad = document.getElementById('input-ciudad');
 
+function cambiarModo(){
+    const btnTema = document.querySelector(".tema-btn");
+    document.body.classList.toggle("light");
+    btnTema.textContent = document.body.classList.contains("light") ? "☀️" : "🌙";
+}
+
+inpCiudad.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        obtenerClima();
+    }
+});
+
 async function obtenerClima(){
     const ciudad = inpCiudad.value;
 
@@ -25,4 +37,23 @@ function mostrarClima(data){
     document.querySelector('.ciudad').innerHTML = data.location.name;
     document.querySelector('.humedad').innerHTML = data.current.humidity + '%';
     document.querySelector('.viento').innerHTML = data.current.wind_kph + 'km/h';
+
+
+    const contenedor = document.querySelector('.contenedor');
+    const condicion = data.current.condition.text.toLowerCase();
+
+    contenedor.classList.remove("lluvia", "nublado", "soleado");
+
+    if (condicion.includes("lluvia")) {
+        contenedor.style.background = "linear-gradient(125deg, #3a7bd5, #3a6073)";
+        contenedor.classList.add("lluvia");
+    } else if (condicion.includes("nublado") || condicion.includes("cloud")) {
+        contenedor.style.background = "linear-gradient(125deg, #757f9a, #d7dde8)";
+        contenedor.classList.add("nublado");
+    } else if (condicion.includes("soleado") || condicion.includes("sunny")) {
+        contenedor.style.background = "linear-gradient(125deg, #fbc2eb, #a6c1ee)";
+        contenedor.classList.add("soleado");
+    } else {
+        contenedor.style.background = "linear-gradient(125deg, #00feba, #5b548a)";
+    }
 }
